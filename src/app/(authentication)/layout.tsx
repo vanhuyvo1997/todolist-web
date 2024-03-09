2217523a8f4d3fa42f6a23f4ef4985ee60e579f6
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-03-09 13:04:52 +0700

    add authentication layout

---
+++ src/app/(authentication)/layout.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+
+export default function AuthenticationLayout({ children }: { children: ReactNode }) {
+  return (
+  <div className="max-w-lg mt-10 mx-auto flex flex-col items-center gap-6">
+    <Image src="/logo.svg" alt="logo" height={86} width={108}/>
+    <div className="rounded-lg py-6 px-6 w-full flex flex-col items-stretch bg-[#EFEFEF] shadow-lg">
+      {children}
+    </div>
+  </div>);
+}
