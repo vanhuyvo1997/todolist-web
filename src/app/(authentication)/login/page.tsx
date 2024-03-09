7b34fc0bcd4a2ab7753f71da863e8ab162cff1bc
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-03-09 13:05:57 +0700

    add login and register pages

---
+++ src/app/(authentication)/login/page.tsx
@@ -0,0 +1,5 @@
+import LoginForm from "@/app/ui/forms/login-form";
+
+export default async function Login() {
+  return <LoginForm/>
+}
---
+++ src/app/(authentication)/register/page.tsx
@@ -0,0 +1,4 @@
+import RegisterForm from '@/app/ui/forms/register-form';
+export default function LoginPage() {
+  return <RegisterForm />;
+}
