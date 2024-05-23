d4628f0285f874352777388331f34630db387ac6
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-05-23 08:26:04 +0700

    add new environment variable

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,11 @@
 namespace NodeJS {
-    interface ProcessEnv {
-      SERVER_URL: string;
-      REGISTER_API: string;
-    }
+  interface ProcessEnv {
+    SERVER_URL: string;
+    REGISTER_API: string;
+    LOGIN_API: string;
+    
+    AUTHORIZATION_SERVER_BASE_URL: string;
+    MYTASK_ID:string;
+    MYTASK_SECRET:string;
   }
+}
