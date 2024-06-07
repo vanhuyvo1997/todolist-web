4d1445ed687b1dd75b58a85f08583938bd92338d
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-06-07 17:11:23 +0700

    update session wrapper

--- src/app/ui/wrappers/SessionProviderWrapper.tsx
+++ src/app/ui/wrappers/SessionProviderWrapper.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { SessionProvider } from "next-auth/react";
+
 
 
 export default function SessionProviderWrapper({ children }: { children: React.ReactNode }) {
-    return <>
+    return <SessionProvider>
         {children}
-    </>;
+    </SessionProvider>;
 }
